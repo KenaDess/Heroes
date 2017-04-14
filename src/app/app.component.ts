8764ab18ecf103c5dc5e09337bff8850a7ce9862
Pauline Dessingue <pdessingue@norsys.fr>
2017-04-14 18:11:03 +0200

    chore(App): initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import {Hero} from './hero';
 
 @Component({
-  selector: 'pr-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pr works!';
+  title : string;
+  myHero : Hero;
+  heroes : Hero[];
+
+  constructor() {
+    this.title = 'Heroes';
+    this.heroes = [
+      new Hero(1, 'Windstorm'),
+      new Hero(13, 'Bombasto'),
+      new Hero(15, 'Magneta'),
+      new Hero(20, 'Tornado')
+    ];
+    this.myHero = this.heroes[0];
+  }
+
 }
