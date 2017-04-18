2f805f87d5d8783aad3be6570b9f46e6d1757b13
Pauline Dessingue <pdessingue@norsys.fr>
2017-04-18 11:18:20 +0200

    feat(HeroService): modify HeroService to return Promise instead of Hero list + add OnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Hero} from './hero';
 import { HeroService } from './hero.service';
+import {isNullOrUndefined, isUndefined} from 'util';
 
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title : string;
   hero : Hero;
   heroes : Hero[];
   selectedHero : Hero;
-  heroService = new HeroService();
 
-  constructor() {
+  constructor(private heroService : HeroService) {
     this.title = 'Tour of Heroes';
-    this.heroes = this.heroService.getHeroes();
-    this.hero = this.heroes[0];
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
 }
