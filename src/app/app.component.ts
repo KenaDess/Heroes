1d96827ac71cf8a0ecf7cd88adc51edd0c2198b3
Pauline Dessingue <pdessingue@norsys.fr>
2017-04-18 10:52:56 +0200

    feat(Details): new component for hero details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,22 @@ import {Hero} from './hero';
 })
 export class AppComponent {
   title : string;
-  myHero : Hero;
+  hero : Hero;
   heroes : Hero[];
+  selectedHero : Hero;
 
   constructor() {
-    this.title = 'Heroes';
+    this.title = 'Tour of Heroes';
     this.heroes = [
       new Hero(1, 'Windstorm'),
       new Hero(13, 'Bombasto'),
       new Hero(15, 'Magneta'),
       new Hero(20, 'Tornado')
     ];
-    this.myHero = this.heroes[0];
+    this.hero = this.heroes[0];
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
